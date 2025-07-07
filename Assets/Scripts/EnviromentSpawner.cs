2931c36867e2fb0e927b1dd077ae0694ba8de69b
Luca Pinciroli <luca.pinciroli02@gmail.com>
2025-07-07 12:07:49 +0200

    Impacchettata arena nel prefab e script per istanziare ambienti

---
+++ Assets/Scripts/EnviromentSpawner.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnvironmentSpawner : MonoBehaviour
+{
+    public GameObject environmentPrefab;
+    public int numEnvironments = 4;
+    public int environmentsPerRow = 2;
+
+    [Tooltip("Spaziatura tra le colonne (asse X)")]
+    public float columnSpacing = 150f;
+
+    [Tooltip("Spaziatura tra le righe (asse Z)")]
+    public float rowSpacing = 60f;
+
+    void Start()
+    {
+        if (environmentPrefab == null)
+        {
+            Debug.LogError("Environment prefab is not assigned!");
+            return;
+        }
+
+        for (int i = 0; i < numEnvironments; i++)
+        {
+            int row = i / environmentsPerRow;
+            int col = i % environmentsPerRow;
+
+            Vector3 position = new Vector3(col * columnSpacing, 0f, row * rowSpacing);
+
+            GameObject env = Instantiate(environmentPrefab, position, Quaternion.identity);
+            env.name = "Environment_" + i;
+        }
+    }
+}
